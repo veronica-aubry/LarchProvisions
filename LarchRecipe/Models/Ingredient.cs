d730bf475ece3ecc2d5324e6e38717d5dd1dff2d
michael smith <michaelanthonysmith00@gmail.com>
2016-03-08 16:32:47 -0800

    add SingleServing method and column display

--- LarchRecipe/Models/Ingredient.cs
+++ LarchRecipe/Models/Ingredient.cs
@@ -9,13 +9,32 @@ namespace LarchRecipe.Models
 {
     public class Ingredient
     {
+        
         public int ID { get; set; }
         public string Name { get; set; }
         public string Source { get; set; }
         public double Amount { get; set; }
         public string Unit { get; set; }
         public int RecipeId { get; set; }
+
+        public double SingleServing()
+        {
+            RecipesDBContext db = new RecipesDBContext();
+            if (this.RecipeId != 0)
+            {
+                Recipe recipe = db.Recipe.Find(this.RecipeId);
+                       
+                double singleServing = this.Amount / recipe.Servings;
+                return singleServing;
+            }
+            else
+            {
+                return 0;
+            }     
+        }
     }
+
+    
 
     public class IngredientDBContext : DbContext
     {
